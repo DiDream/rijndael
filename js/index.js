97259403841d6275f10ccf5d95f647a31616236a
Alex Jimmy <alu0100728170@ull.edu.es>
2017-03-27 04:02:14 +0100

    Empezando con la implementacion del algoritmo Rijndael. Mejoras en el diseño

--- js/index.js
+++ js/index.js
@@ -1,11 +1,57 @@
 'use strict'
 var ROUNDS = 10;
-
-
+var rijndaelResults = $('#rijndael-results');
+var message = [];
 (function(){
-    $('#rijndael-results').append(Input());
+    rijndaelResults.append(Input());
     for(var i=1; i<=ROUNDS; i++){
-        $('#rijndael-results').append(Round(`Ronda ${i}`, `round-${i}`));
+        rijndaelResults.append(Round(`Ronda ${i}`, `round-${i}`));
     }
-    $('#rijndael-results').append(Output());
+    rijndaelResults.append(Output());
 })()
+function InputValues(inputs){
+    var result = [];
+    inputs.each(function(){
+        result.push(parseInt($(this).val(), 16))
+    })
+    return result;
+}
+function setMatrixElements(matrix, values){
+    var i=0;
+    matrix.find('.matrix-element').each(function(){
+        $(this).text(values[i++].toString(16).toUpperCase());
+
+    })
+}
+$('#inputs-form').on('submit',function(e){
+    e.preventDefault();
+    message = InputValues($('#inputs-message input'));
+    var key = InputValues($('#inputs-key input'));
+    var rijndael = new Rijndael(message, key);
+
+
+    //Mostrar Valores de entrada
+    setMatrixElements($('#inputs .start-round'),message)
+    setMatrixElements($('#inputs .round-key'),key)
+
+    //rondas
+    
+});
+
+function RandomValues(){
+    var values = Random();
+    for(var i=0; i<values.length; i++){
+        $(`#m-${i}`).val(values[i]);
+    }
+    values = Random();
+    for(var i=0; i<values.length; i++){
+        $(`#k-${i}`).val(values[i]);
+    }
+}
+function Random(length=16){
+    var result = [];
+    for(var i=0; i<length; i++){
+        result.push(Math.floor(Math.random()*256).toString(16).toUpperCase());
+    }
+    return result;
+}
